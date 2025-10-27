6bb24ffcf316c7b6a3462529bbb679f95c66ce9c
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-10-27 00:20:39 -0300

    feat: add DefaultButton component with icon support and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { PlayCircleIcon } from "lucide-react";
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
@@ -41,7 +43,7 @@ function App() {
             <Cycles />
           </div>
           <div className="formRow">
-            <button type="submit">Start</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>
