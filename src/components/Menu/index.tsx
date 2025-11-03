674a79f0de9f2b97e9520e6f53b5e60a425fd11c
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-11-03 11:03:32 -0300

    feat: implement theme toggle functionality in Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,53 @@
 import { History, House, Settings, Sun } from "lucide-react";
 import style from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export const Menu = () => {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  const handleThemeToggle = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    setTheme(prevTheme => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
   return (
     <nav className={style.menu}>
-      <a className={style.menuLink} href="#">
+      <a
+        className={style.menuLink}
+        href="#"
+        aria-label="Ir para home"
+        title="Ir para a home"
+      >
         <House size={24} />
       </a>
-      <a className={style.menuLink} href="#">
+      <a
+        className={style.menuLink}
+        href="#"
+        aria-label="Ver histórico"
+        title="Ver histórico"
+      >
         <History size={24} />
       </a>
-      <a className={style.menuLink} href="#">
+      <a
+        className={style.menuLink}
+        href="#"
+        aria-label="Ver configurações"
+        title="Ver configurações"
+      >
         <Settings size={24} />
       </a>
-      <a className={style.menuLink} href="#">
+      <a
+        className={style.menuLink}
+        href="#"
+        aria-label="Ativar modo noturno"
+        title="Ativar modo noturno"
+        onClick={handleThemeToggle}
+      >
         <Sun size={24} />
       </a>
     </nav>
